2f73ca23bb982834e73e5316332eb4a8807b9a64
Ivan Simangas <simangasivan@gmail.com>
2025-03-27 03:05:02 -0700

    almost done

--- script.js
+++ script.js
@@ -46,3 +46,89 @@ async function getPartyDetails(id) {
     console.error("Error fetching parties", error);
   }
 }
+
+// === Components ===
+/**
+ * @param {Party} party - the party object to display
+ * @returns {HTMLElement} - a card element displaying the party's details
+ */
+
+function PartyCard(party) {
+  const $card = document.createElement("article");
+  $card.classList.add("party-card");
+  $card.innerHTML = `
+  <h3>${party.name}</h3>
+  <p><strong>Date:</strong>${party.date}</p>
+  <p><strong>Location:</strong>${party.location}</p>
+  <p><strong>Description:</strong>${party.description}</p>
+  <p><strong>Guest List:</strong>${party.guestList}</p>
+  `;
+  return $card;
+}
+
+/**
+ * @returns {HTMLElement} - a list element containing party names
+ */
+
+function PartyList() {
+  const $list = document.createElement("ul");
+  $list.classList.add("party-list");
+
+  parties.forEach((party) => {
+    const $listItem = document.createElement("li");
+    $listItem.textContent = party.name;
+    $listItem.addEventListener("click", () => {
+      getPartyDetails(party.id);
+    });
+    $list.appendChild($listItem);
+  });
+  return $list;
+}
+
+/**
+ * @returns {HTMLElement} - a message prompting the user to select a party
+ */
+
+function NoPartySelected() {
+  const $message = document.createElement("p");
+  $messsage.textContent = "Please select a party to view more details.";
+  return $message;
+}
+
+// === Render ===
+
+function render() {
+  const $app = document.querySelector("#app");
+  $app.innerHTML = `
+  <h1>Party Planner</h1>
+  <main>
+    <section>
+      <h2>
+      ${
+        parties.length > 0
+          ? PartyList().outerHTML
+          : "<p>No parties available</p>"
+      }  <!-- Render list of parties or a message if no parties exist -->
+      </section>
+      <section id="party-details">
+        <h2>Party Details</h2>
+        ${
+          selectedParty
+            ? PartyCard(selectedParty).outerHTML
+            : NoPartySelected().outerHTML
+        }  <!-- Render selected party details or message if no party is selected -->
+      </section>
+      </h2>
+    </section>
+  </main>
+  `;
+}
+
+// === Initialize ===
+
+async function init() {
+  await getParties();
+  render();
+}
+
+init();
