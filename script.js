2acb8ceaf7752103a91ef97e4a4ecd2609e6f374
Ivan Simangas <simangasivan@gmail.com>
2025-03-27 00:08:17 -0700

    added JSDoc

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef Party
+ * @property {number} id - unique identifier for the party
+ * @property {string} name - name of the party
+ * @property {string} description - description of the party
+ * @property {string} date - date of the party
+ * @property {string} location - location where the party will be held
+ * @property {string} guestList - list of guests attending the party
+ */
